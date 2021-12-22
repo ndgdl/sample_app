aaddaf0a4af9897f0d4ae4b28bd7f6534628ba1b
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-12-22 09:26:34 +0100

    user sign up first step

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,8 +12,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
 
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
     get contact_path
     assert_select "title", full_title("Contact")
+    get help_path
+    assert_select "title", full_title("Help")
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
