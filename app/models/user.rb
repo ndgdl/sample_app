c47316a526b208a90b2d4ed20bbd82425fb70b0e
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-12-23 16:27:59 +0100

    added email case sensitive uniquess validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
