09c6338a4159f407cae240b9632ff2ec810119dc
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-04 11:23:56 +0100

    added remember method to sessions helpers and sessions controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Returns the user currently logged in.
   def current_user
     if session[:user_id]
