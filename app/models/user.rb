d88968f934dc357c19d6056268a504b3808200b8
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-04 10:35:13 +0100

    remember methods and token created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,9 +10,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
