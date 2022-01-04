8b227425e516840795681167cc4e4c4cf7d03351
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-04 11:47:46 +0100

    edge case double logout fixed

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -42,6 +42,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     delete logout_path
     assert_not is_logged_in?
+
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     assert_redirected_to root_url
     follow_redirect!
     assert_select "a[href=?]", login_path
