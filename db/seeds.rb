d956c42d2373422beb26baf97061d501fbc78eef
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-05 11:47:47 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Cleaning DB
+p "Cleaning DB..."
+User.destroy_all
+p "Cleaning done"
+
+# Create a main sample user.
+p "Creating main user..."
+User.create!(name:  "Nicolas de Guilhem",
+             email: "nicolas.dgdl@hotmail.fr",
+             password:              "foobar",
+             password_confirmation: "foobar")
+p "Main user created"
+
+# Generate a bunch of additional users.
+p "Creating all other users..."
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password)
+end
+p "All other users created"
