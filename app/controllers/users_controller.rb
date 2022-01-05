b8b7e4039e6c121eb4e692696dbc503d1955907a
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-05 11:20:21 +0100

    users index and view created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update]
+  before_action :logged_in_user, only: %i[index edit update]
   before_action :correct_user, only: %i[edit update]
 
   def new
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
