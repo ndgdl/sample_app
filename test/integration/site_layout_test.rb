84e29850bffcd7abf7e3ce3213697e0787112073
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-05 11:34:37 +0100

    finalized test for layout links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
@@ -13,6 +13,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
 
     get help_path
     assert_select "title", full_title("Help")
@@ -24,5 +25,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Help")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    get users_path(@user)
+    assert_select "title", full_title("All users")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
+    delete logout_path
+    assert_redirected_to root_path
+
   end
 end
