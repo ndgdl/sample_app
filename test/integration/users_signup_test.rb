8eee6aca75d8ec2232fc4ac11d5a0dad477d9420
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-06 11:24:24 +0100

    email sent when user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,8 +23,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    #assert_template 'users/show'
+    #assert is_logged_in?
     assert_not flash.empty?
-    assert is_logged_in?
   end
 end
