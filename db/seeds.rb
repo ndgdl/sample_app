9635e442c65cc0e34d04967cc83c0b68c96f1f5a
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-01-13 17:49:37 +0100

    added relationships to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,13 @@ users = User.order(:created_at).take(6)
   users.each { |user| user.microposts.create!(content: content) }
 end
 p "Microposts created"
+
+p "Creating relationships..."
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+p "Relationships created"
